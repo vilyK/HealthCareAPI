a1a625b8a74ae7bd8684c460e9c00b539e927176
Velichka Karamanova <velichka.k@sbtech.com>
2019-06-07 20:14:12 +0300

    update

--- HealthCare.Models/DatabaseModels/MedicalCenter/Department.cs
+++ HealthCare.Models/DatabaseModels/MedicalCenter/Department.cs
@@ -1,23 +1,11 @@
 ﻿namespace HealthCare.Models.DatabaseModels.MedicalCenter
 {
     using System.Collections.Generic;
-    using User;
 
-    public class MedicalCenterInfo
+    public class Department
     {
         public int Id { get; set; }
-
-        public int UserId { get; set; }
-        public User User { get; set; }
-
-
         public string Name { get; set; }
-
-        public string Details { get; set; }
-
-        public bool IsNzok { get; set; }
-
-        public bool HasEmergency { get; set; }
 
         public List<MedicalCenterDepartment> MedicalCenterDepartments = new List<MedicalCenterDepartment>();
     }
---
+++ HealthCare.Models/DatabaseModels/MedicalCenter/MedicalCenterDepartment.cs
@@ -0,0 +1,13 @@
+﻿namespace HealthCare.Models.DatabaseModels.MedicalCenter
+{
+    public class MedicalCenterDepartment
+    {
+        public int Id { get; set; }
+
+        public int MedicalCenterId { get; set; }
+        public MedicalCenterInfo MedicalCenter { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
