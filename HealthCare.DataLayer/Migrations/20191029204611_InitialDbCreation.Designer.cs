8ad1907c1a10907cf588f86d280fba57935d26bf
vilyK <vily.karamanova@gmail.com>
2019-10-29 22:51:44 +0200

    revert ef core version to 2.4.4

--- HealthCare.DataLayer/Migrations/20191029204611_InitialDbCreation.Designer.cs
+++ HealthCare.DataLayer/Migrations/20191029204611_InitialDbCreation.Designer.cs
@@ -4,14 +4,16 @@ using HealthCare.DataLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HealthCare.DataLayer.Migrations
 {
     [DbContext(typeof(HealthCareDbContext))]
-    partial class HealthCareDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191029204611_InitialDbCreation")]
+    partial class InitialDbCreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
